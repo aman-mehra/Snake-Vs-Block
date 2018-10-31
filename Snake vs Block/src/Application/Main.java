704eceb507df1dd0fe93bfb3a9a194ee470db4b9
Bhavye Anand Gupta <bhavye99@gmail.com>
2018-10-31 05:46:50 +0530

    mainpage and leaderboardpage added

--- Snake vs Block/src/Application/Main.java
+++ Snake vs Block/src/Application/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package Application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -8,16 +8,17 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MainPageScene.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 500, 700));
+        primaryStage.show();
+    }
+
 }
